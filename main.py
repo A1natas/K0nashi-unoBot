b3d4f242e5b727e0ee84e105b6f34d8eeba10889
root <K0nashi@outlook.com>
2021-08-27 13:24:53 +0800

    first push

---
+++ main.py
@@ -0,0 +1,347 @@
+#!python3
+from http.client import METHOD_NOT_ALLOWED
+import json
+from operator import eq
+from random import randint
+from dateutil import rrule
+from urllib.request import urlretrieve
+from PIL import ImageFont,ImageDraw
+from graia.application import Group,Friend
+from graia.application.event.messages import GroupMessage,FriendMessage
+from graia.application.group import Member, MemberInfo
+from graia.application.message.elements import Element
+from graia.application.message.elements.internal import App, At, Image, Plain, Source
+from graia.broadcast import Broadcast
+from graia.application import GraiaMiraiApplication, Session
+from graia.application.message.chain import MessageChain
+import asyncio
+import requests
+from pathlib import Path
+import os
+from PIL import Image as Im
+import sys
+import time
+from datetime import date,timedelta
+import datetime
+import math
+import re,itertools
+from unomanager import Uno_Manager
+
+loop = asyncio.get_event_loop()
+
+bcc = Broadcast(loop=loop)
+app = GraiaMiraiApplication(
+    broadcast=bcc,
+    connect_info=Session(
+        host="http://127.0.0.1:11451", # 填入 httpapi 服务运行的地址
+        authKey="k0nashi114514", # 填入 authKey
+        account=1227057446, # 你的机器人的 qq 号
+        websocket=True # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
+    )
+)    
+testing = 1
+
+created = 0
+gamers = []
+homeowner = 0
+findingGamers= 0
+started = 0
+hand_cards = []
+manager = None
+homegroup = 0
+
+def init():
+    global created,gamers,homeowner,findingGamers,started,hand_cards,manager,homegroup
+    created = 0
+    gamers = []
+    homeowner = 0
+    homegroup = 0
+    findingGamers= 0
+    started = 0
+    manager = None
+
+@bcc.receiver("GroupMessage")
+async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
+    global created,testing,gamers,homeowner,findingGamers,started,hand_cards,manager,homegroup
+    if message.asDisplay() == "uno help":
+        outmsg = """指令列表：\nuno create 创建一场游戏，使用该指令者会被作为房主，管理本场游戏的开始结束。\n我来   当有人使用uno create后，其他人用于加入游戏\nuno start  房主等想来玩的人齐了以后使用该指令开始游戏\nuno over 在create之后如果不玩了记得使用这个指令释放，否则别人也玩不了\n看牌  使用后bot会私聊你的手牌是什么\n游戏命令:\n普通牌/功能牌出牌（例：红9/蓝禁）   如果手中有这张牌，轮到你的时候就可以将这张牌打出\n万能牌出牌（例：变色 黄）   万能牌出牌时需要指定之后的颜色，所以需要在牌的名称后加上颜色的名字\n摸  当你没有东西能出的时候（非禁），可以摸牌\n过  当你上家出了“禁”，而你没有禁可以出的时候，或者你被禁还未解禁的时候，用于跳过本回合 """
+        await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+    if message.asDisplay() == "uno create":
+        if created:
+            outmsg = "有人在玩了，您等等吧"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        else:
+            created = 1
+            findingGamers = 1
+            gamers.append(member)
+            homeowner = member.id
+            homegroup = group.id
+            outmsg = '正在等待其他成员加入。想要一起玩的说一句“我来”就能一起玩哦'
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            outmsg = '人组齐了房主说一声“uno start”即可开始游戏'
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            outmsg = '如果没人或者不玩了，一定要记得“uno over”结束游戏'
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+    if message.asDisplay() == "我来" and findingGamers and group.id == homegroup: 
+        if not member in gamers:
+            gamers.append(member)
+            outmsg = "加入成功！"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        else:
+            outmsg = "你在队里了"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+    if message.asDisplay() == "uno start" and member.id == homeowner:
+        if len(gamers) < 3 and not testing:
+            outmsg = "人不够，您再等等"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        else:
+            outmsg = "游戏开始！"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            findingGamers = 0
+            started = 1
+            manager = Uno_Manager(len(gamers))
+            outmsg = "正在发牌……"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            manager.startUno()
+            outmsg = "发牌完成，发送“看牌”，机器人就会偷偷告诉你你的牌是什么噢！"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))  
+            for eachmambers in gamers:
+                cards = sorted(manager.getHandCards()[gamers.index(eachmambers)],key = lambda x:manager.cardOrder.index(x))
+                outmsg = "你的手牌是：" + "、".join(cards)
+                await app.sendTempMessage(group,eachmambers,MessageChain.create([Plain(outmsg)]))
+                await asyncio.sleep(1)
+    if message.asDisplay() == "看牌" and started and member in gamers and started:   
+        cards = sorted(manager.getHandCards()[gamers.index(member)],key = lambda x:manager.cardOrder.index(x))
+        outmsg = "你的手牌是：" + "、".join(cards)
+        await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+    if message.asDisplay() == "uno over" and (member.id == homeowner or member.id == 562723584) and created:
+        outmsg = "游戏结束！"
+        await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        init()
+    #?出牌
+    if (message.asDisplay() in Uno_Manager.cards or message.asDisplay()[:2] == "+4" or message.asDisplay()[:2] == "变色")and member in gamers and started:
+        if manager.banCheck(gamers.index(member)):
+            outmsg="你被禁了，现在不能出牌"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        if not manager.isTurn(gamers.index(member),message.asDisplay()):
+            outmsg="现在不是你的回合"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        if manager.getCheckAfterTouchFlag():
+            outmsg="你只能选择“出”/“不出”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        if message.asDisplay() in manager.getHandCards()[gamers.index(member)] or message.asDisplay()[:2] in manager.getHandCards()[gamers.index(member)]:
+            cardname = "$$"
+            if message.asDisplay()[:2] =="+4" or message.asDisplay()[:2] == "变色":
+                msg = message.asDisplay().replace(" ","")
+                if len(msg) == 3:
+                    if msg[2] in "红黄蓝绿":
+                        cardname = msg[:2]
+                        color = msg[2]
+                else:
+                    outmsg="功能牌请输入要转换的颜色，例如“变色 黄”"
+                    await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+                    return
+            else:
+                cardname = message.asDisplay()
+                color = None
+            if manager.outCard(gamers.index(member),cardname,color):
+                outmsg = member.name + " 出了一张：" + manager.getLastCard()
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+                manager.lastOneCheck()
+                manager.turnNext()
+                winnerId = manager.winCheck()
+                if winnerId != -1:
+                    outmsg = "游戏结束，获胜者：" + member.name
+                    await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+                    init()
+                    return
+                outmsg = "现在轮到：" + gamers[manager.getTurn()].name + "出牌了，Ta还剩" + str(len(manager.getHandCards()[manager.getTurn()])) + "张牌"
+                if manager.getLastCard() != "":
+                    outmsg += ",上一张牌是：" + manager.getLastCard()
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+                if manager.banCheck(manager.getTurn()):
+                    outmsg = gamers[manager.getTurn()].name + "还有" + str(manager.getBanDict(manager.getTurn())) + "回合解禁，请选择“过”"
+                    await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+                cards = sorted(manager.getHandCards()[gamers.index(member)],key = lambda x:manager.cardOrder.index(x))
+                outmsg = "你的手牌是：" + "、".join(cards)
+                await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+                nextmember = gamers[manager.getTurn()]
+                cards = sorted(manager.getHandCards()[gamers.index(nextmember)],key = lambda x:manager.cardOrder.index(x))
+                outmsg = "你的手牌是：" + "、".join(cards)
+                await app.sendTempMessage(group,nextmember,MessageChain.create([Plain(outmsg)]))
+            else:
+                outmsg = "你不能出这个哦，上一张牌是：" + manager.getLastCard()
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        else:
+            outmsg = "你出的什么jb？"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+    
+    if message.asDisplay() == "摸" and member in gamers and started:
+        if not manager.isTurn(gamers.index(member)):
+            outmsg="现在不是你的回合"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        if manager.getBanFlag() > 0:
+            outmsg="现在不能摸，你只能出“禁”或者“过”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        if manager.getCheckAfterTouchFlag():
+            outmsg="你只能选择“出”/“不出”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        touchNum = manager.touch(gamers.index(member))
+        outmsg = member.name + "摸了" + str(touchNum) + "张牌"
+        await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        if manager.checkAfterTouch(touchNum):
+            card = manager.getHandCards()[gamers.index(member)][-1]
+            outmsg = "你抽到了一张：" + card + "，请在群里选择“出”/“不出”"
+            await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+            outmsg = "请选择“出”/“不出”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        manager.turnNext()
+        outmsg = "现在轮到：" + gamers[manager.getTurn()].name + "出牌了，Ta还剩" + str(len(manager.getHandCards()[manager.getTurn()])) + "张牌"
+        if manager.getLastCard() != "":
+            outmsg += ",上一张牌是：" + manager.getLastCard()
+        await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        if manager.banCheck(manager.getTurn()):
+            outmsg = gamers[manager.getTurn()].name + "还有" + str(manager.getBanDict(manager.getTurn())) + "回合解禁，请选择“过”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        cards = sorted(manager.getHandCards()[gamers.index(member)],key = lambda x:manager.cardOrder.index(x))
+        outmsg = "你的手牌是：" + "、".join(cards)
+        await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+        nextmember = gamers[manager.getTurn()]
+        cards = sorted(manager.getHandCards()[gamers.index(nextmember)],key = lambda x:manager.cardOrder.index(x))
+        outmsg = "你的手牌是：" + "、".join(cards)
+        await app.sendTempMessage(group,nextmember,MessageChain.create([Plain(outmsg)]))
+
+    if message.asDisplay() == "过" and member in gamers and started:
+        if not manager.isTurn(gamers.index(member)):
+            outmsg="现在不是你的回合"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        skipFlag = manager.couldSkip()
+        if skipFlag != 0:
+            if (skipFlag & 0b10) != 0:
+                outmsg= member.name + "选择了跳过，被禁了" + str(manager.getBanFlag()) + "回合"
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            manager.banCount()
+            if (skipFlag & 0b01) != 0:
+                outmsg= member.name + "还要被禁" + str(manager.getBanDict(manager.getTurn())) + "回合"
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            manager.turnNext()
+            outmsg = "现在轮到：" + gamers[manager.getTurn()].name + "出牌了，Ta还剩" + str(len(manager.getHandCards()[manager.getTurn()])) + "张牌"
+            if manager.getLastCard() != "":
+                outmsg += ",上一张牌是：" + manager.getLastCard()
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            if manager.banCheck(manager.getTurn()):
+                outmsg = gamers[manager.getTurn()].name + "还有" + str(manager.getBanDict(manager.getTurn())) + "回合解禁，请选择“过”"
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            cards = sorted(manager.getHandCards()[gamers.index(member)],key = lambda x:manager.cardOrder.index(x))
+            outmsg = "你的手牌是：" + "、".join(cards)
+            await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+            nextmember = gamers[manager.getTurn()]
+            cards = sorted(manager.getHandCards()[gamers.index(nextmember)],key = lambda x:manager.cardOrder.index(x))
+            outmsg = "你的手牌是：" + "、".join(cards)
+            await app.sendTempMessage(group,nextmember,MessageChain.create([Plain(outmsg)]))
+            return
+        else:
+            outmsg= "你只有在“禁”之后，才能选择跳过"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+
+    if message.asDisplay() == "出" or message.asDisplay() == "不出" or (message.asDisplay()[:2] == "出 " and message.asDisplay()[2] in "红黄蓝绿" and len(message.asDisplay()) == 3) and manager.getCheckAfterTouchFlag() and started and member in gamers:
+        if not manager.isTurn(gamers.index(member)):
+            outmsg="现在不是你的回合"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        if not manager.getCheckAfterTouchFlag():
+            outmsg="你出nm呢？"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            return
+        msg = manager.getHandCards()[gamers.index(member)][-1]
+        if len(message.asDisplay()) == 3:
+            msg += " " + message.asDisplay()[2]
+        print(msg)
+        if message.asDisplay()[0] == "出" and member == gamers[manager.getTurn()]:
+            if msg[:2] =="+4" or msg[:2] == "变色":
+                msg = msg.replace(" ","")
+                if len(msg) == 3:
+                    if msg[2] in "红黄蓝绿":
+                        cardname = msg[:2]
+                        color = msg[2]
+                else:
+                    outmsg="功能牌请输入要转换的颜色，例如“出 黄”"
+                    await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+                    return
+            else:
+                cardname = msg
+                color = None
+            if manager.outCard(gamers.index(member),cardname,color):
+                manager.lastOneCheck()
+                outmsg = member.name + " 出了一张：" + manager.getLastCard()
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        if message.asDisplay() == "不出" and member == gamers[manager.getTurn()]:
+            pass
+        manager.resetCheckAfterTouchFlag()
+        manager.turnNext()
+        outmsg = "现在轮到：" + gamers[manager.getTurn()].name + "出牌了，Ta还剩" + str(len(manager.getHandCards()[manager.getTurn()])) + "张牌"
+        if manager.getLastCard() != "":
+            outmsg += ",上一张牌是：" + manager.getLastCard()
+        await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        if manager.banCheck(manager.getTurn()):
+            outmsg = gamers[manager.getTurn()].name + "还有" + str(manager.getBanDict(manager.getTurn())) + "回合解禁，请选择“过”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        cards = sorted(manager.getHandCards()[gamers.index(member)],key = lambda x:manager.cardOrder.index(x))
+        outmsg = "你的手牌是：" + "、".join(cards)
+        await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+        nextmember = gamers[manager.getTurn()]
+        cards = sorted(manager.getHandCards()[gamers.index(nextmember)],key = lambda x:manager.cardOrder.index(x))
+        outmsg = "你的手牌是：" + "、".join(cards)
+        await app.sendTempMessage(group,nextmember,MessageChain.create([Plain(outmsg)]))
+        return
+    if message.asDisplay() == "uno":
+        if not created:
+            outmsg = "想来把uno吗？“uno create”即可开启一场游戏哦！想了解游戏规则可以使用“uno help”"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+        else:
+            if not member in gamers:
+                return
+            unoFlag = manager.getLastOneCheckFlag()
+            if unoFlag == -1:
+                outmsg = "你uno nm呢？"
+                await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)])) 
+                return
+            if gamers.index(member) == unoFlag:
+                manager.resetLastOneCheckFlag()
+            else:
+                print("uno error")
+
+    if message.asDisplay() == "没说uno" and started and member in gamers:
+        lastOneCheckFlag = manager.getLastOneCheckFlag()
+        if lastOneCheckFlag != -1:
+            manager.gamerDraw(lastOneCheckFlag,2)
+            outmsg = gamers[lastOneCheckFlag].name + "忘喊uno，被罚两张"
+            manager.resetLastOneCheckFlag()
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            cards = sorted(manager.getHandCards()[lastOneCheckFlag],key = lambda x:manager.cardOrder.index(x))
+            outmsg = "你的手牌是：" + "、".join(cards)
+            await app.sendTempMessage(group,gamers[lastOneCheckFlag],MessageChain.create([Plain(outmsg)]))
+        else:
+            print(gamers.index(member))
+            # manager.gamerDraw(gamers.index(member),1)
+            # outmsg = "乱出警是吧，赏你一张"
+            outmsg = "乱出警是吧，爬"
+            await app.sendGroupMessage(group,MessageChain.create([Plain(outmsg)]))
+            cards = sorted(manager.getHandCards()[gamers.index(member)],key = lambda x:manager.cardOrder.index(x))
+            outmsg = "你的手牌是：" + "、".join(cards)
+            await app.sendTempMessage(group,member,MessageChain.create([Plain(outmsg)]))
+        
+app.launch_blocking()
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    exit()
---
+++ unomanager.py
@@ -0,0 +1,222 @@
+from asyncio.events import Handle
+import random
+
+from pydantic.parse import load_str_bytes
+
+class Uno_Manager:
+    cards=["红1","红2","红3","红4","红5","红6","红7","红8","红9","红0",
+        "红1","红2","红3","红4","红5","红6","红7","红8","红9",
+        "蓝1","蓝2","蓝3","蓝4","蓝5","蓝6","蓝7","蓝8","蓝9","蓝0",
+        "蓝1","蓝2","蓝3","蓝4","蓝5","蓝6","蓝7","蓝8","蓝9",
+        "黄1","黄2","黄3","黄4","黄5","黄6","黄7","黄8","黄9","黄0",
+        "黄1","黄2","黄3","黄4","黄5","黄6","黄7","黄8","黄9",
+        "绿1","绿2","绿3","绿4","绿5","绿6","绿7","绿8","绿9","绿0",
+        "绿1","绿2","绿3","绿4","绿5","绿6","绿7","绿8","绿9",
+        "红禁","红禁","绿禁","绿禁","黄禁","黄禁","蓝禁","蓝禁",
+        "红转","红转","绿转","绿转","黄转","黄转","蓝转","蓝转",
+        "红+2","红+2","绿+2","绿+2","黄+2","黄+2","蓝+2","蓝+2",
+        "变色","变色","变色","变色",
+        "+4","+4","+4","+4",
+    ]
+    cardOrder = [
+    "红0","红1","红2","红3","红4","红5","红6","红7","红8","红9","红禁","红转","红+2",
+    "黄0","黄1","黄2","黄3","黄4","黄5","黄6","黄7","黄8","黄9","黄禁","黄转","黄+2",
+    "蓝0","蓝1","蓝2","蓝3","蓝4","蓝5","蓝6","蓝7","蓝8","蓝9","蓝禁","蓝转","蓝+2",
+    "绿0","绿1","绿2","绿3","绿4","绿5","绿6","绿7","绿8","绿9","绿禁","绿转","绿+2",
+    "变色","+4",
+    ]
+    def __init__(self,gamers):
+        self.gamers = gamers
+    
+    def draw(self,num):
+        if num <= len(self.gaming_cards):
+            pass
+        else:
+            new_cards = Uno_Manager.cards
+            random.shuffle(new_cards)
+            self.gaming_cards.extend(new_cards)
+        draw_card = self.gaming_cards[:num]
+        self.gaming_cards = self.gaming_cards[num:]
+        print(draw_card)
+        print(self.gaming_cards)
+        return draw_card
+        # return ["变色"] + ["变色"] 
+
+    def gamerDraw(self,gamerId,num):
+        print(gamerId)
+        self.hand_cards[gamerId].extend(self.draw(num))
+        print(self.hand_cards[gamerId])
+
+    def startUno(self):
+        self.gaming_cards = Uno_Manager.cards
+        random.shuffle(self.gaming_cards)
+        self.hand_cards = []
+        for i in range(self.gamers):
+            self.hand_cards.append(self.draw(5))
+        self.lastCard = ""
+        self.plusNum = 0
+        self.lastPlus = 0
+        self.turnId = 0
+        self.turnForward = 1
+        self.banFlag = 0
+        self.banDict = {}
+        self.checkAfterTouchFlag = False
+        self.unoFlag = {}
+        self.lastOneCheckFlag = -1
+
+    def getHandCards(self):
+        return self.hand_cards
+
+    def getCardOrder(self):
+        return self.cardOrder
+
+    def legalCheck(self,lastCard,cardname):
+        if self.lastPlus != 0 and not '+' in cardname:
+            return False
+        if self.lastPlus == 4 and "+2" in cardname:
+            return False
+        if "禁" in lastCard and not "禁" in cardname and self.banFlag != 0:
+            return False
+        if cardname == "+4":
+            return True
+        if cardname == "变色":
+            return True
+        if lastCard != "":
+            if lastCard[0] != cardname[0] and lastCard[1] != cardname[1]:
+                return False
+        return True
+
+    def changelastCard(self,cardname,color=None):
+        if color != None:
+            self.lastCard = color + "$"
+        else:
+            self.lastCard = cardname
+        if "+" in cardname:
+            if cardname == "+4":
+                self.lastPlus = 4
+                self.plusNum +=4
+            else:
+                self.lastPlus = 2
+                self.plusNum +=2
+
+    def getLastCard(self):
+        if "$" in self.lastCard:
+            if self.lastPlus == 0:
+                cardname = "变色 " + self.lastCard[0]
+            else:
+                cardname = "+4 " + self.lastCard[0]
+        else:
+            cardname = self.lastCard
+        return cardname
+
+    def outCard(self,gamerId,cardname,color=None):
+        if self.lastCard == "":
+            pass
+        else:
+            if not self.legalCheck(self.lastCard,cardname):
+                return False
+            else: 
+                pass
+        print(self.hand_cards)
+        print(cardname)
+        self.hand_cards[gamerId].remove(cardname)
+        self.changelastCard(cardname,color)
+        if "转" in cardname:
+            self.turnForward *= -1
+        if "禁" in cardname:
+            self.banFlag += 1
+        if "0" or "7" in cardname:
+            pass
+        return True
+    
+    def winCheck(self):
+        for i in range(len(self.hand_cards)):
+            if len(self.hand_cards[i]) == 0:
+                return i
+        return -1
+
+    def isTurn(self,gamerId,outCard = "$$"):
+        if gamerId == self.turnId:
+            return True
+        elif outCard == self.lastCard:
+            self.turnId = gamerId
+            return True
+        return False
+
+    def turnNext(self):
+        self.turnId = (self.turnId + self.turnForward + len(self.hand_cards)) % len(self.hand_cards)
+    
+
+    def getTurn(self):
+        return self.turnId
+
+    def touch(self,gamerId):
+        if self.plusNum != 0:
+            num = self.plusNum
+            self.plusNum = 0
+            self.lastPlus = 0
+        else:
+            num = 1
+        self.hand_cards[gamerId].extend(self.draw(num)) 
+        return num
+
+    def getBanFlag(self):
+        return self.banFlag
+
+    def banCheck(self,gamerId):
+        return gamerId in self.banDict.keys()
+
+    def banAdd(self,gameId):
+        self.banDict[gameId] += self.banFlag
+        self.banFlag = 0     
+
+    def getBanDict(self,gamerId):
+        if gamerId in self.banDict.keys():
+            return  self.banDict[gamerId]
+        else:
+            return 0
+
+    def banCount(self):
+        if self.banFlag > 0:
+            if self.getTurn() in self.banDict.keys():
+                self.banDict[self.getTurn()] += self.banFlag
+            else:
+                self.banDict[self.getTurn()] = self.banFlag
+            self.banFlag = 0
+        self.banDict[self.getTurn()] -= 1
+        if self.banDict[self.getTurn()] == 0:
+            del self.banDict[self.getTurn()]
+    
+    def couldSkip(self):
+        skipFlag = 0b00
+        if self.banCheck(self.getTurn()):
+            skipFlag |= 0b01
+        if self.banFlag != 0:
+            skipFlag |= 0b10
+        return skipFlag
+        
+    def checkAfterTouch(self,touchNum):
+        if touchNum > 1:
+            return False
+        if self.legalCheck(self.lastCard,self.hand_cards[self.getTurn()][-1]):
+            self.checkAfterTouchFlag = True
+            return True
+        return False
+
+    def getCheckAfterTouchFlag(self):
+        return self.checkAfterTouchFlag
+        
+    def resetCheckAfterTouchFlag(self):
+        self.checkAfterTouchFlag = False
+
+    def lastOneCheck(self):
+        if len(self.hand_cards[self.getTurn()]) == 1:
+            self.lastOneCheckFlag = self.getTurn()
+        else:
+            self.lastOneCheckFlag = -1
+    def resetLastOneCheckFlag(self):
+        self.lastOneCheckFlag = -1
+    
+    def getLastOneCheckFlag(self):
+        return self.lastOneCheckFlag
+        
